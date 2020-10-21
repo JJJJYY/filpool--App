26d00fad4da8e076d13f17d58ccfc68f3874d373
JJJJYY <843418890@qq.com>
2020-10-21 15:19:32 +0800

    添加轮播图组件

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,11 +1,61 @@
 import React from 'react';
-import { View, Text, } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper'
+import { getGeneralBannerApi } from '../../request/api/homeApi'
 export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    componentDidMount() {
+        getGeneralBannerApi().then(res => {
+            console.log(res)
+        })
+    }
+
     render() {
         return (
-            <View>
-                <Text>home</Text>
+            <View style={styles.wrapper}>
+                <Swiper showsButtons={false}>
+                    <View style={styles.slide1}>
+                        <Text style={styles.text}>Hello Swiper</Text>
+                    </View>
+                    <View style={styles.slide2}>
+                        <Text style={styles.text}>Beautiful</Text>
+                    </View>
+                    <View style={styles.slide3}>
+                        <Text style={styles.text}>And simple</Text>
+                    </View>
+                </Swiper>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    wrapper: {
+        height: 150
+    },
+    slide1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9DD6EB'
+    },
+    slide2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#97CAE5'
+    },
+    slide3: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#92BBD9'
+    },
+    text: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold'
+    }
+})
